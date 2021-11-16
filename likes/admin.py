164aac60290d2f4ba53ddd0398e3ed8fe754285a
Alemnew Marie <alemnewmarie461@gmail.com>
2021-11-16 09:56:53 +0300

    Modify likes app to refer custom user model

--- likes/admin.py
+++ likes/admin.py
@@ -1,5 +1,1 @@
 from django.contrib import admin
-
-from .models import LikedItem
-
-admin.site.register(LikedItem)
--- likes/models.py
+++ likes/models.py
@@ -1,5 +1,5 @@
+from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
@@ -8,7 +8,7 @@ class LikedItem(models.Model):
     """Items liked by a user."""
 
     # what user likes what object
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
